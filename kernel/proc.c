cad31f17d72c6e539fad7feba89dfa5a20c47a47
root <bbad1323@126.com>
2023-04-28 13:51:54 +0800

    main_1

--- kernel/proc.c
+++ kernel/proc.c
@@ -165,7 +165,7 @@ freeproc(struct proc *p)
   p->xstate = 0;
   p->state = UNUSED;
 
-  p->trace_mask = 0;
+
 }
 
 // Create a user page table for a given process,
@@ -297,7 +297,6 @@ fork(void)
   // Cause fork to return 0 in the child.
   np->trapframe->a0 = 0;
 
-  np->trace_mask = p->trace_mask; //here
 
   // increment reference counts on open file descriptors.
   for(i = 0; i < NOFILE; i++)
